45813b353c15c117acb3324c21d9957fb90e0179
丸岡 晴海 <harumi.maruoka.zz@gmail.com>
2025-11-24 16:16:14 +0900

    UnitySampleを追加

---
+++ UnitySample/Assets/ExcelMaster/Scripts/Editor/GenerateSource.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace ExcelMaster
+{
+    public class GenerateSource
+    {
+        [MenuItem("Examples/ExcelMaster/Generate Source from Excel")]
+        public static void GenerateExample()
+        {
+            string excelFilePath = "Assets/ExcelMaster/Data/Excels/ItemData.xlsx";
+            string className = "ItemData";
+            string tableName = "Item";
+            string outputDirectory = "Assets/ExcelMaster/Scripts/Generated";
+
+            Generate(
+                excelFilePath: excelFilePath,
+                className: className,
+                tableName: tableName,
+                outputDirectory: outputDirectory);
+        }
+
+
+        public static void Generate(string excelFilePath, string className, string tableName, string outputDirectory)
+        {
+            var excelArray = CsvConverter.ExcelToArray(
+                excelFilePath: excelFilePath,
+                sheetIndex: 1,
+                startRow: 1,
+                startColumn: 1);
+
+            var classSource = CsvClassGenerator.Parse(
+                usingNamespaces: new string[]
+                {
+                    "System",
+                    "System.Collections.Generic",
+                    "UnityEngine",
+                    "ExcelMaster",
+                    "MasterMemory",
+                    "MessagePack",
+                },
+
+                classAttributes: new string[]
+                {
+                    $"[MemoryTable(\"{tableName}\")]",
+                    "[MessagePackObject(true)]",
+                },
+
+                @namespace: "GameData",
+                className: className,
+                csv: excelArray);
+
+            var builderSource = MasterMemoryBinaryGenerator.Generate(
+                usingNamespaces: Array.Empty<string>(),
+                @namespace: "GameData",
+                buildClassName: className + "Builder",
+                masterClassName: className);
+
+            var classFilePath = System.IO.Path.Combine(outputDirectory, className + ".cs");
+            var builderFilePath = System.IO.Path.Combine(outputDirectory, className + "Builder.cs");
+
+            System.IO.Directory.CreateDirectory(outputDirectory);
+            System.IO.File.WriteAllText(classFilePath, classSource);
+            System.IO.File.WriteAllText(builderFilePath, builderSource);
+        }
+
+    }
+}
